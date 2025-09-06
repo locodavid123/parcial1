7afa0ba20311a24823ede05876fac8314b8bcb9e
locodavid123 <locojuancho20@hotmail.com>
2025-09-06 10:50:47 -0500

    admin productos

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -36,9 +36,9 @@ export default function SuperUserDashboard() {
                         }
                     />
                     <AdminCard
-                        href="/superUser/productos"
+                        href="/admin/productos"
                         title="Gestión de Productos"
-                        description="Añadir nuevos productos, actualizar precios y gestionar el stock."
+                        description=" actualizar precios y gestionar el stock."
                         icon={
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
